2dd5ba97fdaa27e75d3a7cc9bbffac586553fa68
abtion <abtion@outlook.com>
2018-06-14 16:01:37 +0800

    update script of extracting samples

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # event-extraction
 event extraction in traditional ways
+
+## extract samples
+
+1. extract sentences for ace chinese data set.
+2. classify the sentences in following types:
+
+|0|1|2|3|4|5|6|7|8|
+|:-:|:-:|:-:|:-:|:-:|:-:|:-:|:-:|:-:|
+|not any class|life|movement|transaction|business|conflict|contact|personnel|justice|
+
+## power by
+
+- ace chinese data set
+- lxml
--- extract_samples.py
+++ extract_samples.py
@@ -1,18 +1,24 @@
 import re
 import os
+import random
 from lxml import etree
 
 
 def main():
-    output = open('samples/samples.txt', 'w', encoding='utf-8')
+    train_set = open('samples/train_set.txt', 'w', encoding='utf-8')
+    ver_set = open('samples/ver_set.txt', 'w', encoding='utf-8')
+    test_set = open('samples/test_set.txt', 'w', encoding='utf-8')
     types = {'Life': '1', 'Movement': '2', 'Transaction': '3', 'Business': '4',
              'Conflict': '5', 'Contact': '6', 'Personnel': '7', 'Justice': '8'}
 
     paths = ['/Users/abtion/workspace/dataset/ace/Chinese/bn/adj/',
              '/Users/abtion/workspace/dataset/ace/Chinese/nw/adj/',
              '/Users/abtion/workspace/dataset/ace/Chinese/wl/adj/']
+    tests, verifys = _get_test_and_verify_set(paths)
+
     for path in paths:
         files = os.listdir(path)
+        random.shuffle(files)
         for file in files:
             if re.search(r'\.apf\.xml', file):
                 full_txt_file = file[:-7] + 'sgm'
@@ -30,7 +36,8 @@ def main():
                                 seq = seq.replace('\n', '')
                                 seq = seq.replace(' ', '')
                                 temp_text = (types[type] + '\t' + seq + '。\n')
-                                output.write(temp_text)
+                                # output.write(temp_text)
+                                _output(file, verifys, tests, train_set, ver_set, test_set, temp_text)
 
                 full_txt = full_txt.replace('\n', '')
                 full_txt = full_txt.replace(' ', '')
@@ -39,17 +46,46 @@ def main():
                     seq = re.sub(r'^[：，“”"「」\s]', '', seq)
                     if seq != '':
                         temp_text = '0' + '\t' + seq + '。\n'
-                        output.write(temp_text)
+                        _output(file, verifys, tests, train_set, ver_set, test_set, temp_text)
+
+
+def _get_test_and_verify_set(paths):
+    files = []
+    verifies = []
+    tests = []
+    for path in paths:
+        files.extend(os.listdir(path))
+    random.shuffle(files)
+
+    for file in files:
+        if re.search(r'\.apf\.xml', file):
+            if len(verifies) < 33:
+                verifies.append(file)
+            elif len(tests) < 66:
+                tests.append(file)
+            else:
+                break
+    return tests, verifies
+
+
+def _output(file, verifies, tests, train, ver_set, test_set, seq):
+    if file in verifies:
+        ver_set.write(seq)
+        train.write(seq)
+    elif file in tests:
+        test_set.write(seq)
+    else:
+        train.write(seq)
 
 
 def _get_text(path, file):
     body = etree.parse(path + file).getroot().find('BODY')
     if path[-7:-5] == 'bn':
-        return body.xpath('//text()')[-6]
+        return body.xpath('//TURN[1]/text()')[0]
     if path[-7:-5] == 'nw':
-        return body.xpath('//text()')[-3]
+        return body.xpath('//TEXT[1]/text()')[0]
     if path[-7:-5] == 'wl':
-        return body.xpath('//text()')[-4]
+        return body.xpath('//POST[1]/text()')[2]
 
 
 if __name__ == '__main__':
